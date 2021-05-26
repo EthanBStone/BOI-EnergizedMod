3ee1ead562618f277205c30661c37feff9b32c44
EthanBStone <72464126+EthanBStone@users.noreply.github.com>
2021-05-25 17:16:12 -0700

    Bug fixes and changes
    --Made it so hearts are only converted when you are close to them
    --Fixed coop bugs where transformation wasnt being tracked properly

--- main.lua
+++ main.lua
@@ -7,7 +7,6 @@ local json = require("json")
 
 local COSTUME_ENERGIZED = Isaac.GetCostumeIdByPath("gfx/characters/EnergizedCostume.anm2")
 
-local transformProgress = {0, 0, 0, 0}
 local transformItems = { 
 	647, --4.5 volt
 	603, --Battery pack
@@ -19,22 +18,24 @@ local transformItems = {
 }
 
 function BatteryMod:onUpdate()
+	for i = 1, game:GetNumPlayers() do
+		local currplayer = game:GetPlayer(i)
+			currplayer:GetData().pNum = i
+	end		
 	--Start of game
-	if game:GetFrameCount() == 1 then
+	--[[if game:GetFrameCount() == 1 then
 		--print("New run started")
 		
-	end
-
+	]]--end
 
 end
 
 local GameState = {}
-function BatteryMod:onStart()
+
+function BatteryMod:onStart(continuedRun)
 	GameState = json.decode(BatteryMod:LoadData() )
-	if GameState.RoomCount == nil then GameState.RoomCount = 0 end
-	if GameState.LevelCount == nil then GameState.LevelCount = 0 end
-	if GameState.Transformed == nil then GameState.Transformed = {0,0,0,0} end
-	if GameState.Room == nil then GameState.Room = {0} end
+	if GameState.Transformed == nil or continuedRun == false then GameState.Transformed = {0,0,0,0} end
+	if GameState.TransformProgress == nil or continuedRun == false then GameState.TransformProgress = {0,0,0,0} end
 end
 
 function BatteryMod.onGameExit() 
@@ -43,122 +44,88 @@ end
 -- Check for transformations every second or so
 function BatteryMod:onPlayerUpdate(player)
 	local pNum = 1
-	--Find the playernum of the current player
-	for i = 1, game:GetNumPlayers() do
-		if game:GetPlayer(i) == player then
-			pNum = i
-		end
-	end	
-	if game:GetFrameCount() % 30 == 1 then
-		BatteryMod:CheckTransformations(player, pNum)
-		--for i, ent in pairs(Isaac.FindInRadius(player.Position, 50, EntityPartition.PICKUP)) do	
-			--print("Entity index = " .. ent.Index)
-		--end
+	
+	if not (player:GetData().pNum == nil) then 
+		pNum = player:GetData().pNum
 	end
 	
-	for i, ent in pairs(Isaac.FindInRadius(player.Position, 10, EntityPartition.PICKUP)) do
-		if ent:GetSprite():IsPlaying("Collect") and ent:GetData().Picked == nil and ent.Variant == PickupVariant.PICKUP_LIL_BATTERY and GameState.Transformed[pNum] == 1 then
-			ent:GetData().Picked = true
-			
-			if ent.SubType == BatterySubType.BATTERY_MICRO then
-				player:AddHearts(1)
-			else 
-				player:AddHearts(2)	
-			end
-			--print("Entity picked up!")
-		end	
+	--Check transformation for each player
+	if game:GetFrameCount() % 30 == 1 then
+			BatteryMod:CheckTransformations(player, pNum)	
 	end
+	
+	if not(GameState.Transformed == nil) and GameState.Transformed[pNum] == 1  then
+		--Transform hearts into batteries
+		for i, pickup in pairs(Isaac.FindInRadius(player.Position, 45, EntityPartition.PICKUP)) do
+			pickup = pickup:ToPickup()
+			if pickup.Variant == PickupVariant.PICKUP_HEART and pickup:GetData().EnergizedTF == nil  then		
+				pickup:GetData().EnergizedTF = true
+				--Replace half heart
+				if pickup.SubType == HeartSubType.HEART_HALF then
+					pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, true, true, false)
+				--Replace full heart
+				elseif pickup.SubType == HeartSubType.HEART_FULL then
+					pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, true, true, false)
+					Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, pickup.Position, Vector(0.25,-0.25), nil)
+				--Replace double heart
+				elseif pickup.SubType == HeartSubType.HEART_DOUBLEPACK then
+					pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, true, true, false)
+					Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, pickup.Position, Vector(0.25,-0.25), nil)
+					Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, pickup.Position, Vector(0.25,-0.25), nil)
+				end
+			end
+		end
+	
+				
+		--Heal on battery pickup
+		for i, ent in pairs(Isaac.FindInRadius(player.Position, 10, EntityPartition.PICKUP)) do
+			--print("Check P" .. pNum .. " Transformed = " .. GameState.Transformed[pNum])
+			if ent:GetSprite():IsPlaying("Collect") and ent:GetData().Picked == nil and ent.Variant == PickupVariant.PICKUP_LIL_BATTERY and GameState.Transformed[pNum] == 1 then
+				ent:GetData().Picked = true
+				if ent.SubType == BatterySubType.BATTERY_MICRO then
+					player:AddHearts(1)
+				else 
+					player:AddHearts(2)	
+				end
+				
+			end	
+		end
 	
 	
 end
 
+end
+
 function BatteryMod:CheckTransformations(player, pNum)
 	--Reset Transformation Status
-	transformProgress[pNum] = 0
+	GameState.TransformProgress[pNum] = 0
 	for k in pairs(transformItems)  do
 		local curItemCount = player:GetCollectibleNum(transformItems[k], IgnoreModifiers)
 		if curItemCount > 0 then
 			--print("Current item: " .. transformItems[k] ..", has " .. player:GetCollectibleNum(transformItems[k], IgnoreModifiers))
-			transformProgress[pNum] = transformProgress[pNum] + curItemCount
+			GameState.TransformProgress[pNum] = GameState.TransformProgress[pNum] + curItemCount
 		end
 	end
 	--Activate transformation
-	if transformProgress[pNum] >= 3 and GameState.Transformed[pNum] == 0 then
+	if GameState.TransformProgress[pNum] >= 3 and GameState.Transformed[pNum] == 0 then
 		--print("P" .. pNum .. " has transformed!")
 		GameState.Transformed[pNum] = 1
 		sound:Play(SoundEffect.SOUND_POWERUP_SPEWER)
 		piber20HelperMod:doStreak("Energized!")
 		player:AddNullCostume(COSTUME_ENERGIZED)
 	--Take away transformation if player rerolls out of it or loses the items
-	elseif transformProgress[pNum] < 3 and GameState.Transformed[pNum] == 1 then
+	elseif GameState.TransformProgress[pNum] < 3 and GameState.Transformed[pNum] == 1 then
 		--print("P" .. pNum .. " has untransformed :(")
 		GameState.Transformed[pNum] = 0
+		player:TryRemoveNullCostume(COSTUME_ENERGIZED)
 	end
-	--print("Transform progress" .. transformProgress[1])
+	--print("Transform progress" .. GameState.TransformProgress[1])
 end
 
-local roomVisitCount = 0
-function BatteryMod:onPickupUpdate(pickup)
-	if pickup.Variant == PickupVariant.PICKUP_HEART and pickup:GetData().BatteryRolled == nil and roomVisitCount < 2 and (GameState.Transformed[1] == 1 or GameState.Transformed[2] == 1 or GameState.Transformed[3] == 1 or GameState.Transformed[4] == 1) then
-		local batteryRNG = pickup:GetDropRNG()
-		local rngVal = batteryRNG:RandomInt(100)
-		local replaceFlag = false
-		if rngVal <= 90 then
-			replaceFlag = true
-		end
-		pickup:GetData().BatteryRolled = true		
-		--Replace half heart
-		if pickup.SubType == HeartSubType.HEART_HALF and replaceFlag then
-			pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, true, true, false)
-		--Replace full heart
-		elseif pickup.SubType == HeartSubType.HEART_FULL and replaceFlag then
-			pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, true, true, false)
-			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, pickup.Position, Vector(0.25,-0.25), nil)
-		--Replace double heart
-		elseif pickup.SubType == HeartSubType.HEART_DOUBLEPACK and replaceFlag then
-			pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, true, true, false)
-			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, pickup.Position, Vector(0.25,-0.25), nil)
-			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, pickup.Position, Vector(0.25,-0.25), nil)
-		end
-	
-	end
 
 
-end
- 
-function BatteryMod:onNewRoom()
-	roomVisitCount = game:GetLevel():GetCurrentRoomDesc().VisitedCount
-	StageRoomCount = 0
-	if GameState.Room[1] == nil or GameState.Room[1] == 0 then
-		StageRoomCount = 0
-	else
-		for i, ind in pairs(GameState.Room) do 
-			StageRoomCount = StageRoomCount + 1
-		end
-	end
-	local isDupe = false
-	for i, ind in pairs(GameState.Room) do
-		if(game:GetLevel():GetCurrentRoomDesc().ListIndex) == ind then
-			isDupe = true
-		end
-	
-	end
-	if not isDupe then
-		GameState.Room[StageRoomCount + 1] = game:GetLevel():GetCurrentRoomDesc().ListIndex
-		print("New room added " .. game:GetLevel():GetCurrentRoomDesc().ListIndex)
-	else
-		print("Dupe found, not adding " .. game:GetLevel():GetCurrentRoomDesc().ListIndex)
-	end
-	
-end
 
-function BatteryMod:onNewLevel()
-	GameState.Room = {0}
-end
-BatteryMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, BatteryMod.onStart)
+BatteryMod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, BatteryMod.onStart)
 BatteryMod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, BatteryMod.onPlayerUpdate)
 BatteryMod:AddCallback(ModCallbacks.MC_POST_UPDATE, BatteryMod.onUpdate)
-BatteryMod:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, BatteryMod.onPickupUpdate)
-BatteryMod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, BatteryMod.onNewRoom)
-BatteryMod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, BatteryMod.onNewLevel)
 BatteryMod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, BatteryMod.onGameExit)
